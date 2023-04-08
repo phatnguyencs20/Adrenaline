830e4d1818856e5044ba048b5b94c304df161a78
Phát Nguyễn <phatnt@Phaticisms-MacBook-Pro.local>
2023-04-08 13:48:41 +0700

    appbar, navbar, rooms added

---
+++ IntelliHome/screens/FaceRecognition/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+const FaceRecognition = () => {
+    return (
+        <View>
+            <Text>Face Recognition</Text>
+        </View>
+    )
+};
+
+export default FaceRecognition;
---
+++ IntelliHome/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LivingRoom = () => {
+    return (
+        <View>
+            <Text>Living Room</Text>
+        </View>
+    )
+};
+
+export default LivingRoom;
