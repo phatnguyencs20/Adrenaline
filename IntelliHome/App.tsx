e3a1a9f01acf0c62ffd33dd863fbbd0e3c07f240
Phát Nguyễn <phatnt@Phaticisms-MacBook-Pro.local>
2023-04-09 17:48:03 +0700

    profile page and login page added

--- IntelliHome/App.tsx
+++ IntelliHome/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Provider as PaperProvider } from 'react-native-paper';
 
-import NavBar from "./components/NavBar";
-import AppBar from "./components/Appbar";
-import Button from "./components/Button";
-import Bedroom from "./screens/Bedroom";
+import Login from "./screens/Login";
+import Profile from "./screens/Profile";
 
 export default function App() {
   return (
     <PaperProvider>
-      <AppBar/>
-      <NavBar />
+      <Profile />
     </PaperProvider>
   );
 }
