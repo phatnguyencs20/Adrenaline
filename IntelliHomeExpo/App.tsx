f4ca119bd262ebf4e03e3906a414031a9bef8421
Phát Nguyễn <phatnt@Phaticisms-MacBook-Pro.local>
2023-04-10 17:19:43 +0700

    signup and settting page added

--- IntelliHomeExpo/App.tsx
+++ IntelliHomeExpo/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Provider as PaperProvider } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
+import { Provider as PaperProvider, DefaultTheme, MD3DarkTheme } from 'react-native-paper';
 
 import Bedroom from "./screens/Bedroom";
 import FaceRecognition from "./screens/FaceRecognition";
@@ -7,11 +7,18 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import Profile from "./screens/Profile";
 import Setting from "./screens/Setting";
+import Signup from "./screens/Signup";
 
 export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [theme, setTheme] = useState(DefaultTheme);
+
+  useEffect(() => {
+    setTheme(isDarkMode ? MD3DarkTheme : DefaultTheme);
+  }, [isDarkMode]);
   return (
-    <PaperProvider>
-      <Profile />
+    <PaperProvider theme={theme}>
+      <Setting isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
     </PaperProvider>
   );
 }
