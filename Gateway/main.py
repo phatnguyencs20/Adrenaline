f4d90c8d0b0fd0abc84132d7b74af06039c2830e
phatnguyencs20 <phat.nguyencs20@hcmut.edu.vn>
2023-04-22 22:14:10 +0700

    simulate temp and humidity

--- Gateway/main.py
+++ Gateway/main.py
@@ -1,97 +1,50 @@
-import serial.tools.list_ports
-import random
 import time
-import  sys
-from  Adafruit_IO import  MQTTClient
+import math
+import requests
 
-AIO_FEED_IDS = ["humidity", "temperature", "minifan"]
-AIO_USERNAME = "phatnt"
-AIO_KEY = "aio_xVna17f5ZfmsGHob3HMGeZ7dryiT"
+ADAFRUIT_IO_KEY = 'aio_xVna17f5ZfmsGHob3HMGeZ7dryiT'
+ADAFRUIT_IO_USERNAME = 'phatnt'
 
-def connected(client):
-    print("Ket noi thanh cong...")
-    for feed in AIO_FEED_IDS:
-        client.subscribe(feed)
+TEMPERATURE_FEED = 'temperature'
+HUMIDITY_FEED = 'humidity'
 
-def  subscribe(client , userdata , mid , granted_qos):
-    print("Subscribe thanh cong...")
+TEMPERATURE_UNIT = 'C'
+HUMIDITY_UNIT = '%'
 
-def  disconnected(client):
-    print("Ngat ket noi...")
-    sys.exit (1)
-
-def  message(client , feed_id , payload):
-    print("Nhan du lieu: " + payload)
-    if isMicrobitConnected:
-        ser.write((str(payload) + "#").encode())
-
-client = MQTTClient(AIO_USERNAME , AIO_KEY)
-client.on_connect = connected
-client.on_disconnect = disconnected
-client.on_message = message
-client.on_subscribe = subscribe
-client.connect()
-client.loop_background()
-
-def getPort():
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range(0, N):
-        port = ports[i]
-        strPort = str(port)
-        if "USB Serial Device" in strPort:
-            splitPort = strPort.split(" ")
-            commPort = (splitPort[0])
-    return commPort
-
-isMicrobitConnected = False
-if getPort() != "None":
-    ser = serial.Serial( port=getPort(), baudrate=115200)
-    isMicrobitConnected = True
-
-ser = serial.Serial(port = getPort(), baudrate = 115200)
-
-mes = ""
-temp_data = 0
-def processData(data):
-    data = data.replace("!", "") // !
-    data = data.replace("#", "") // #
-    splitData = data.split(":") // #:
-    print(splitData)
-    if splitData[1] == "TEMP":
-        client.publish("temperature", splitData[2])
-        temp_data = splitData[2]
-
-fan_on = True
-fan_off = False
-def isFanRun(fan_signal):
-    if fan_signal == False 
-        client.publish ("minifan", fan_off)
-    elif fan_signal == True
-        client.publish ("minifan", fan_on)
-
-def TempToFan(temp_data):
-    if temp_data >= 25 & temp_data <= 30
-        isFanRun(1)
-        time.sleep(5)
-
-mes = ""
-def readSerial():
-    bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
-        global mes
-        mes = mes + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mes) and ("!" in mes):
-            start = mes.find("!")
-            end = mes.find("#")
-            processData(mes[start:end + 1])
-            if (end == len(mes)):
-                mes = ""
-            else:
-                mes = mess[end+1:]
+# Set the initial temperature and time
+temperature = 25.0
+humidity = 50.0
+start_time = time.time()
 
 while True:
-    if isMicrobitConnected:
-        readSerial()
-    time.sleep(1)
+    # Calculate the temperature and humidity values based on sine and cosine curves, respectively
+    elapsed_time = time.time() - start_time
+    temperature = round(25.0 + 10.0 * math.sin(elapsed_time / 120.0), 2)
+    humidity = round(50.0 + 20.0 * math.cos(elapsed_time / 120.0), 2)
+
+    # Create the data payloads for the Adafruit feeds
+    temperature_data = {'value': temperature, 'unit': TEMPERATURE_UNIT}
+    humidity_data = {'value': humidity, 'unit': HUMIDITY_UNIT}
+
+    # Make the HTTP requests to update the Adafruit feeds
+    temperature_url = 'https://io.adafruit.com/api/v2/{0}/feeds/{1}/data'.format(
+        ADAFRUIT_IO_USERNAME, TEMPERATURE_FEED)
+    humidity_url = 'https://io.adafruit.com/api/v2/{0}/feeds/{1}/data'.format(
+        ADAFRUIT_IO_USERNAME, HUMIDITY_FEED)
+
+    headers = {'X-AIO-Key': ADAFRUIT_IO_KEY,
+               'Content-Type': 'application/json'}
+
+    temperature_response = requests.post(
+        temperature_url, headers=headers, json=temperature_data)
+    humidity_response = requests.post(
+        humidity_url, headers=headers, json=humidity_data)
+
+    # Print the response status codes and temperature/humidity values
+    print('Temperature Response:', temperature_response.status_code,
+          'Temperature:', temperature, TEMPERATURE_UNIT)
+    print('Humidity Response:', humidity_response.status_code,
+          'Humidity:', humidity, HUMIDITY_UNIT)
+
+    # Wait for 3 seconds before updating the feeds again
+    time.sleep(3)
